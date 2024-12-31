59f7e027d7c1131b27875854559f94f2b20840f4
ArvindaShiva <rgarvindashiva@gmail.com>
2024-12-31 15:08:38 +0530

    Add files via upload

---
+++ Add2num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Add2num{
+  public static void main(String args[]) {
+    int x,y;
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Enter num1:");
+    x=obj.nextInt();
+    System.out.println("Enter num2:");
+    y=obj.nextInt();
+    int sum=x+y;
+    System.out.println("The Sum is:"+sum);
+   }
+ }
+
+
+    
