eb5373cfdb642ba12911ff39f59cfcabfa91d31b
ArvindaShiva <rgarvindashiva@gmail.com>
2025-01-07 11:59:17 +0530

    Add files via upload

---
+++ Exercise2-Java/Multiply.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Multiply{
+	public static void main(String[] args){
+		int a=12;
+		int b=13;
+		int c=a*b;
+		System.out.print("The Product: ");
+		System.out.print(c);
+}
+
+}
