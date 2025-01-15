8bd6f0db257b7c9360c2698fb226127c74177ef0
ArvindaShiva <rgarvindashiva@gmail.com>
2025-01-15 22:10:19 +0530

    Add files via upload

---
+++ Exercise2-Java/Stu.java
@@ -0,0 +1,13 @@
+public class Stu {
+  public static void main(String[] args) {
+    String name="Arvinda Shiva";
+    int rno=34;
+    String dept="CSE";
+    String sec="A";
+    System.out.println("Student details:");
+    System.out.println("Name: " + name);
+    System.out.println("Roll Number: " + rno);
+    System.out.println("Department: " + dept);
+    System.out.println("Section: " + sec);
+  }
+}
