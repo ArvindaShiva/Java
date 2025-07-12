477407d2ad244f74bf6881235b8f29968b0aea6e
Arvinda Shiva R G <rgarvindashiva@gmail.com>
2025-07-12 19:16:55 +0530

    Update and rename Alt Num to Alt Num.java

--- Exercise2-Java/Alt Num.java
+++ Exercise2-Java/Alt Num.java
@@ -1,5 +1,5 @@
-public static void main(String[] args){
-   public class Alt Num{
+public class Alt Num{
+  public static void main(String[] args){
       int num=10;
       for(int i=1;i<num;i=i+2){
          System.out.println(i);
