bddf05814d13b79de72ec9999a80aad975f2330d
Arvinda Shiva R G <rgarvindashiva@gmail.com>
2025-08-02 22:14:48 +0530

    Add files via upload

---
+++ Java(Array)/sumofodd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sumofodd{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number of elements");
+    int num=sc.nextInt();
+    int Arr[]= new int[num];
+    for(int i=0;i<num;i++){
+       Arr[i]=sc.nextInt();
+  }
+    int sum=0;
+    for(int i=1;i<num;i=i+2){
+       sum+=Arr[i];
+   }
+   System.out.println("Sum of elements present in odd place: "+ sum)
+ }
+}
